b2509ae5bb769e6843a0054ddc21b3488d5a926d
felix <felixchoy97@gmail.com>
2022-05-17 18:48:53 -0700

    Added shake effect to image, portfolio page wip

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 var audio = new Audio("smacksoundeffect.mp3");
-audio.volume = 0.75;
-function smackSound(){
-    var playAudio = function(){
-      audio.loop = false;
-      audio.play();
-    }
 
-    document.getElementById("random-button").addEventListener("click", playAudio);
-  }
+audio.loop = false;
+audio.volume = 0.7;
+
+function smackSound(){
+  audio.currentTime = 0.5; //start audio 0.5s in
+  let kronii =  document.getElementById("kronii-image");
+
+  //animate shaking when button is pressed
+  kronii.animate([    
+    { transform: 'translate(1px, 1px) rotate(0deg)' },
+    { transform: 'translate(-1px, -2px) rotate(-1deg)', offset: 0.1 },
+    { transform: 'translate(-3px, 0px) rotate(1deg)', offset: 0.2 },
+    { transform: 'translate(3px, 2px) rotate(0deg)', offset: 0.3 },
+    { transform: 'translate(1px, -1px) rotate(1deg)', offset: 0.4 },
+    { transform: 'translate(-1px, 2px) rotate(-1deg)', offset: 0.5 },
+    { transform: 'translate(-3px, 1px) rotate(0deg)', offset: 0.6 },
+    { transform: 'translate(3px, 1px) rotate(-1deg)', offset: 0.7 },
+    { transform: 'translate(-1px, -1px) rotate(1deg)', offset: 0.8 },
+    { transform: 'translate(1px, 2px) rotate(0deg)', offset: 0.9 },
+    { transform: 'translate(1px, -2px) rotate(-1deg)' }], {duration: 300, iterations: 1});
+
+  audio.play();
+}
